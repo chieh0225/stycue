5136386bf129e1d263a97a50deba9adbdd4344b2
sunset3777 <aua52257@gmail.com>
2026-07-05 20:35:31 +0800

    feat: add home layout with bottom navigation

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-        StyleCue
-      </h1>
-    </div>
-  );
-}
