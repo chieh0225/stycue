f543ab233894c415bfe435ae7e54b9a663fde9b9
sunset3777 <aua52257@gmail.com>
2026-07-05 20:38:21 +0800

    feat: add points center pages

---
+++ src/app/(main)/profile/points/page.tsx
@@ -0,0 +1,8 @@
+export default function PointsPage() {
+  return (
+    <div className="flex flex-1 flex-col gap-6 p-6">
+      <h1 className="text-2xl font-semibold text-black dark:text-zinc-50">積分中心</h1>
+      {/* 取得目前積分、每日登入積分、取得積分方案 */}
+    </div>
+  );
+}
