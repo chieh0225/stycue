2f91332d2ce7f422ac533b0d0d7897122ba57549
sunset3777 <aua52257@gmail.com>
2026-07-05 20:34:08 +0800

    feat: add auth route group with login and register pages

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPasswordPage() {
+  return (
+    <div className="flex flex-col gap-6">
+      <h1 className="text-2xl font-semibold text-black dark:text-zinc-50">忘記密碼</h1>
+    </div>
+  );
+}
